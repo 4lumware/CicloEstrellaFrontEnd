f1946e891e78cf1bfae584c6ea2d17149607bf02
Carlo050 <u202315055@upc.edu.pe>
2025-10-30 02:05:58 -0500

    adding login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import {Layout} from './layout/public/layout';
 import {LandingComponent} from './pages/public/landing/landing-component';
 import {AuthenticatedUserLayout} from './layout/private/user/authenticated-user-layout';
 import {UserHomeComponent} from './pages/private/user-home/user-home.component';
+import {Login} from './pages/public/login/login';
+import {HeaderAuth} from './layout/header-auth/header-auth';
 
 
 export const routes: Routes = [
@@ -12,5 +14,6 @@ export const routes: Routes = [
   {path: 'private', component: AuthenticatedUserLayout, children:[
       {path: 'home', component: UserHomeComponent}
     ]},
+  {path: 'login', component: Login}
 ];
 
