fce4024a23d7af2bee98aa0353657e8f3fc1a8c3
Yamiii23 <u202313344@upc.edu.pe>
2025-11-04 02:03:18 -0500

    feat:Añadí la sección Formality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { Routes } from '@angular/router';
 import { Layout } from './layout/public/layout';
 import { LandingComponent } from './pages/public/landing/landing-component';
 import { AuthenticatedUserLayout } from './layout/private/user/authenticated-user-layout';
-import { UserHomeComponent } from './pages/private/user-home/user-home.component';
 import { Login } from './pages/public/login/login';
 import { HeaderAuth } from './layout/header-auth/header-auth';
 import { Register } from './pages/public/register/register';
-import { UserProfesoresComponent } from './pages/private/user-home/user-profesores/user-profesores.component';
 import { AuthenticatedAdminDashboard } from './layout/private/admin/authenticated-admin-dashboard/authenticated-admin-dashboard';
 import { UserManagement } from './pages/private/admin/user-management/user-management';
 import { AdminDashboardHome } from './pages/private/admin/admin-dashboard-home/admin-dashboard-home';
+import {Formality} from './pages/private/user/formality/formality';
+import {UserProfesoresComponent} from './pages/private/user/user-profesores/user-profesores.component';
+import {UserHomeComponent} from './pages/private/user/user-home.component';
 
 export const routes: Routes = [
   { path: '', component: Layout, children: [{ path: '', component: LandingComponent }] },
@@ -19,6 +20,7 @@ export const routes: Routes = [
     children: [
       { path: 'home', component: UserHomeComponent },
       { path: 'profesores', component: UserProfesoresComponent },
+      { path: 'tramites', component: Formality }
     ],
   },
   {
---
+++ src/app/pages/private/user/formality/formality.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ChatbotComponent} from '../../../../components/chatbot/chatbot.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatIconButton, MatMiniFabButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-formality',
+  imports: [
+    CommonModule,
+    ChatbotComponent,
+    MatIconModule,
+    MatIconButton,
+    MatMiniFabButton
+  ],
+  templateUrl: './formality.html',
+  styleUrl: './formality.css',
+})
+export class Formality {
+  chatbotVisible = false;
+  userInput = '';
+  suggestions = [
+    'Consultar profesores',
+    'Ver biblioteca',
+    'Trámites académicos'
+  ];
+
+  toggleChatbot() {
+    this.chatbotVisible = !this.chatbotVisible;
+  }
+}
